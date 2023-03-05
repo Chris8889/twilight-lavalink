12b2de34117aa1b748403909778f7fe09f9a9288
Chris <67835702+Chris8889@users.noreply.github.com>
2023-03-05 18:32:51 +0000

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![warn(
+    clippy::missing_const_for_fn,
+    clippy::pedantic,
+    missing_docs,
+    unsafe_code
+)]
+#![allow(
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::unnecessary_wraps
+)]
+
+pub mod client;
+pub mod model;
+pub mod node;
+pub mod player;
+
+#[cfg(feature = "http-support")]
+pub mod http;
+
+pub use self::{client::Lavalink, node::Node, player::PlayerManager};
