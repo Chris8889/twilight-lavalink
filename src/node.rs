d0994c0810b9c0de3357e3e1b41fddba5277d0dd
Chris <67835702+Chris8889@users.noreply.github.com>
2023-03-05 19:00:43 +0000

    Update node.rs

--- src/node.rs
+++ src/node.rs
@@ -627,7 +627,7 @@ impl Drop for Connection {
 }
 
 fn connect_request(state: &NodeConfig) -> Result<Request<()>, NodeError> {
-    let mut request = "ws://lavalink84.fly.dev"
+    let mut request = format!("ws://{}", state.address)
         .into_client_request()
         .map_err(|source| NodeError {
             kind: NodeErrorType::BuildingConnectionRequest,
